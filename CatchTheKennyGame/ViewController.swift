4aad533633ecb34402e5331bd81cce58ab162abd
hamitseyrek <hamitseyrek@gmail.com>
2022-01-14 21:15:57 +0300

    add counter with timer

--- CatchTheKennyGame/ViewController.swift
+++ CatchTheKennyGame/ViewController.swift
@@ -9,6 +9,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // view
     @IBOutlet weak var timeLabel: UILabel!
     @IBOutlet weak var scoreLabel: UILabel!
     @IBOutlet weak var highScoreLabel: UILabel!
@@ -22,12 +23,16 @@ class ViewController: UIViewController {
     @IBOutlet weak var kenny2: UIImageView!
     @IBOutlet weak var kenny1: UIImageView!
     
+    // variables
     var score = 0
+    var timer = Timer()
+    var counter = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
         scoreLabel.text = "Score: \(score)"
         
+        //images tap
         kenny1.isUserInteractionEnabled = true
         kenny2.isUserInteractionEnabled = true
         kenny3.isUserInteractionEnabled = true
@@ -57,12 +62,34 @@ class ViewController: UIViewController {
         kenny7.addGestureRecognizer(recognizer7)
         kenny8.addGestureRecognizer(recognizer8)
         kenny9.addGestureRecognizer(recognizer9)
+        
+        // Timers
+        counter = 10
+        timeLabel.text = String(counter)
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(countDown), userInfo: nil, repeats: true)
          
     }
 
     @objc func increaseScore(){
         score += 1
         scoreLabel.text = "Score: \(score)"
+    }
+    
+    @objc func countDown(){
+        counter -= 1
+        timeLabel.text = String(counter)
+        if counter == 0 {
+            timer.invalidate()
+            // Alert
+            let alert = UIAlertController(title: "Time's up", message: "Do you want to play again?", preferredStyle: UIAlertController.Style.alert)
+            let playAgain = UIAlertAction(title: "Yes", style: UIAlertAction.Style.default) {
+                (UIAlertAction) in
+            }
+            let cancel = UIAlertAction(title: "No", style: UIAlertAction.Style.cancel, handler: nil)
+            alert.addAction(cancel)
+            alert.addAction(playAgain)
+            self.present(alert, animated: true, completion: nil)
+        }
     }
 }
 
